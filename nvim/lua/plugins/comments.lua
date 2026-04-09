55a38b009cf981aac9731f53a7a6120b3c0717bd
sandipduley <sandipp0987rahul@gmail.com>
2026-04-09 12:04:17 +0530

    FIX: fallback to commentstring for single-language filetypes

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,16 +1,28 @@
--- Comment lines or visual selections easily
+-- lua/plugins/comments.lua
 return {
-  'numToStr/Comment.nvim',
-  opts = {},
-  config = function()
-    local key_opts = { noremap = true, silent = true }
+	"numToStr/Comment.nvim",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
 
-    -- Normal mode toggles
-    vim.keymap.set('n', '<C-/>', require('Comment.api').toggle.linewise.current, key_opts)
+		local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 
-    -- Visual mode toggles
-    local visual_toggle = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"
-    vim.keymap.set('v', '<C-/>', visual_toggle, key_opts)
-  end,
+		require("Comment").setup({
+			pre_hook = function(ctx)
+				-- ts_context_commentstring returns nil for single-language files (Go, Python, etc.)
+				-- fall back to vim's native commentstring in that case
+				return ts_pre_hook(ctx) or vim.bo.commentstring
+			end,
+		})
+
+		local key_opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<C-/>", require("Comment.api").toggle.linewise.current, key_opts)
+
+		local visual_toggle = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"
+		vim.keymap.set("v", "<C-/>", visual_toggle, key_opts)
+	end,
 }
-

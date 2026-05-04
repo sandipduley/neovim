0fb2f60b7f3de5e009122d7eabb2161d3981109c
Sandip Duley <sandipp0987rahul@gmail.com>
2026-05-05 03:25:04 +0530

    improved(indent-blankline): enhanced

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,31 +1,87 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	event = { "BufReadPre", "BufNewFile" }, -- lazy-load instead of on startup
 
-  opts = {
-    -- Character used for indentation
-    indent = {
-      char = "▏",
-    },
+	opts = {
+		indent = {
+			char = "▏",
+			tab_char = "▏", -- consistent char for tabs too
+		},
 
-    -- Scope settings
-    scope = {
-      show_start = false,       -- Do not highlight start of scope
-      show_end = false,         -- Do not highlight end of scope
-      show_exact_scope = false, -- Do not highlight the exact scope
-    },
+		whitespace = {
+			remove_blankline_trail = true, -- clean up trailing whitespace on blank lines
+		},
 
-    -- Exclude certain filetypes from indent guides
-    exclude = {
-      filetypes = {
-        "help",
-        "startify",
-        "dashboard",
-        "packer",
-        "neogitstatus",
-        "NvimTree",
-        "Trouble",
-      },
-    },
-  },
+		scope = {
+			enabled = true,
+			show_start = false,
+			show_end = false,
+			show_exact_scope = false,
+			-- highlight scope line with a distinct color via treesitter
+			highlight = { "IBLScope" },
+			include = {
+				node_type = {
+					-- scope detection for common languages
+					lua = { "table_constructor", "function", "if_statement", "for_statement" },
+					python = { "function_definition", "class_definition", "for_statement", "if_statement" },
+					go = { "func_literal", "function_declaration", "if_statement", "for_statement" },
+					javascript = { "arrow_function", "function_declaration", "object", "if_statement" },
+					typescript = { "arrow_function", "function_declaration", "object", "if_statement" },
+				},
+			},
+		},
+
+		exclude = {
+			-- filetypes that don't benefit from indent guides
+			filetypes = {
+				"", -- unnamed buffers
+				"help",
+				"man",
+				"lazy",
+				"mason",
+				"dashboard",
+				"alpha",
+				"startify",
+				"neo-tree",
+				"NvimTree",
+				"Trouble",
+				"trouble",
+				"notify",
+				"toggleterm",
+				"TelescopePrompt",
+				"TelescopeResults",
+				"packer",
+				"neogitstatus",
+				"gitcommit",
+				"lspinfo",
+			},
+			-- buffer types that should never have indent guides
+			buftypes = {
+				"terminal",
+				"nofile",
+				"quickfix",
+				"prompt",
+				"acwrite",
+			},
+		},
+	},
+
+	config = function(_, opts)
+		local ibl = require("ibl")
+		local hooks = require("ibl.hooks")
+
+		-- Custom highlight groups so indent lines don't fight your colorscheme
+		vim.api.nvim_set_hl(0, "IBLIndent", { fg = "#3b4048", nocombine = true })
+		vim.api.nvim_set_hl(0, "IBLScope", { fg = "#9C3028", nocombine = true })
+		opts.indent.highlight = { "IBLIndent" }
+
+		ibl.setup(opts)
+
+		-- Re-apply highlights after colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "IBLIndent", { fg = "#3b4048", nocombine = true })
+			vim.api.nvim_set_hl(0, "IBLScope", { fg = "#6b737e", nocombine = true })
+		end)
+	end,
 }

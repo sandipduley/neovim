e93fc70d5d379fdef869bccd9a360031859b8137
0xlichi <sandipp0987rahul@gmail.com>
2026-05-18 15:12:10 +0530

    Change escape key mapping to force quit on save

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -63,7 +63,7 @@ vim.keymap.set("n", "<C-i>", "<C-i>", opts)
 
 -- ── Insert mode escape ────────────────────────────────────────────────────────
 vim.keymap.set("i", "<Esc>", "<Esc>:w<CR>", opts)
-vim.keymap.set("n", "<Esc>", "<Esc>:wqa<CR>", opts)
+vim.keymap.set("n", "<Esc>", "<Esc>:wqa!<CR>", opts)
 vim.keymap.set("i", "jj", "<Esc>:w<CR>", opts)
 vim.keymap.set("i", "kk", "<Esc>:wq<CR>", opts)
 
